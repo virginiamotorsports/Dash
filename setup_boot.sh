41131d941bd1ce427403f834bd1aa99cf1116a81
John Link <jwl9vq@virginia.edu>
2023-03-20 10:37:12 -0400

    it works again

--- setup_boot.sh
+++ setup_boot.sh
@@ -1,1 +1,1 @@
-ros2 run robot_upstart install fastdash/launch/launch_dash.launch.py --job dashboard --symlink --setup /home/va/Dash/install/setup.bash --user va
+ros2 run robot_upstart install fastdash/launch/launch_dash.launch.py --rosdistro humble --job dashboard --symlink --setup /home/va/Dash/install/setup.bash --user va
